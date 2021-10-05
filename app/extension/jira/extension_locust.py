4b7e272cbb118896f21d15e8a169f0d7376dd1ec
kushal189 <88141624+kushal189@users.noreply.github.com>
2021-10-05 10:44:21 +0530

    Update extension_locust.py

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -45,8 +45,6 @@ def app_specific_action(locust):
      content_post = r_post.content.decode('utf-8')
      logging.error("content post" + content_post)
      logging.info("content post" + content_post)
-     logger.locust_info("r post" + r_post)
-     logger.error("r post" + r_post)
     
 #     token_pattern_example = '"token":"(.+?)"'
 #     id_pattern_example = '"id":"(.+?)"'
