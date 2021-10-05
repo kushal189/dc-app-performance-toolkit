d6893b4a5841790dea24481812d077a0be91ab40
kushal189 <88141624+kushal189@users.noreply.github.com>
2021-10-05 17:42:35 +0530

    Update extension_locust.py

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -20,31 +20,6 @@ def app_specific_action(locust):
      if 'sc' not in content_get_sc:
          logger.error(f"'sc' was not found in {content_get_sc}")
      assert 'sc' in content_get_sc
-               
-     body = {
-                 "accessKey": "f4235a42ea1e4bc2051ffdf1249877ed2ffa9392fcc53767e66943ce998605c1",
-                 "address": "cloud.tenable.com",
-                 "assignee": "unassigned",
-                 "buttonClicked": "save",
-                 "customQuery": "undefined",
-                 "enableSync": "true",
-                 "encryptedSecretKey": "ZTNkOGMwOTFlNGNjNGQ3NTA0NGUwNzMzOGQ1MzBjZDNlMzJlMmVkMTgxMjFkOGRmNDA1MmIwYTRlY2Q3OTNjYgk=",
-                 "interval": "60", 
-                 "product": "io", 
-                 "project": "TESTIO", 
-                 "proxyPassword": "",
-                 "proxySetup": "no",
-                 "proxyUrl": "",
-                 "proxyUsername": "",
-                 "reporter": "admin",
-                 "severity": "info", 
-                 "syncSince": "08/18/2021 15:01",
-                 "verifySSL": "undefined"
-            }
-     r_post = locust.client.put('/rest/tenable/1.0', body, catch_response=True)
-     content_post = r_post.content.decode('utf-8')
-     logging.error("content post" + content_post)
-     logging.info("content post" + content_post)
     
 #     token_pattern_example = '"token":"(.+?)"'
 #     id_pattern_example = '"id":"(.+?)"'
